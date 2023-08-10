75d71f897fae12995609be99e8fcdf27373fdee5
charity20 <mcharity980@gmail.com>
2023-08-10 16:33:41 -0700

    task 6

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+for digit in range (10):
+    for one_digit in range(digit + 1, 10):
+        print("{:02d}".format(digit * 10 + one_digit), end=", " if digit < 8 or one_digit < 9 else "\n")
