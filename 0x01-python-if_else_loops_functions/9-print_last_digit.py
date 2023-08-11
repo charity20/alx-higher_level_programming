47cf1dc3f9cc530107785058a42749f7e854d740
charity20 <mcharity980@gmail.com>
2023-08-10 17:03:21 -0700

    hot fix

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    if number > 0:
-        last_digit = number % 10
-    else:
-        last_digit = number % -10
-    return (last_digit)
+    if number < 0:
+        number = -number
+    print(number % 10, end="")
+    return(number % 10)
