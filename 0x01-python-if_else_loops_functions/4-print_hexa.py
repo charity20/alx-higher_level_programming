8383ab232c9ef40f947d425be3aaa34f7a7908ed
charity20 <mcharity980@gmail.com>
2023-08-10 17:43:09 -0700

    hot fix

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 for num in range(99):
-    print("{:<3 = 0x{:02X}".format(num, num))
+    print("{:<3} = 0x{:02X}".format(num, num))
--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-for digit in range(10):
-    for one_digit in range(digit + 1, 10):
-        print("{:02d}".format(digit * 10 + one_digit), end=", " 
-                if digit < 8 or one_digit < 9 
-                else "\n")
+for i in range(0, 9):
+    for j in range(i + 1, 10):
+        if i == 8:
+            print("{}{}".format(i, j))
+        else:
+            print("{}{}".format(i, j), end=", ")
