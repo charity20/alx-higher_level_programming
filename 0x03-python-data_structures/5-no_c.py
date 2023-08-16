209d0f9a30747ca7415b7402c137de915e7b7b3a
charity20 <mcharity980@gmail.com>
2023-08-15 18:12:46 -0700

    hot fix

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
     new_string = ''
-    for i in range(len(my_string)):
-        if my_string[i] != 'c' & my_string[i] != 'C':
-            new_string += my_string[i]
+    for i in my_string:
+        if i != 'c' & i != 'C':
+            new_string += i
     return (new_string)
