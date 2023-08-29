456b87eb87c684667adbf08b59e65631e365a7bd
charity20 <mcharity980@gmail.com>
2023-08-29 15:23:02 -0700

    task 1 and 0

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+"""A class that defines a square."""
+class square:
+    """Defines a square. """
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a class square."""
+
+class square:
+    """Defines the square size as a private attribute."""
+    def __init__(self, size):
+        """Initialize the size of the square
+        Args:
+        size (int): size of the square. """
+        self.__size = size
