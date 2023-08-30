72c2aa04899769aaec16666b7f797764b359410b
charity20 <mcharity980@gmail.com>
2023-08-30 16:10:33 -0700

    Task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,9 +6,5 @@ def safe_print_list(my_list=[], x=0):
         for i in range(x):
             print(my_list[i], end="")
             print_elements += 1
-
-    except:
-        pass
-
     print()
     return print_elements
