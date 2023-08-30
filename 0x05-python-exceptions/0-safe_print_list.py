2adc8beabea8e0b8861556b022312ecba35e5c28
charity20 <mcharity980@gmail.com>
2023-08-30 16:15:22 -0700

    Hot fix!

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,5 +6,7 @@ def safe_print_list(my_list=[], x=0):
         for i in range(x):
             print(my_list[i], end="")
             print_elements += 1
+    except IndexError:
+        pass
     print()
     return print_elements
