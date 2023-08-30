9227a7fdd2f0b49e2d284b95317a6da398d777b8
charity20 <mcharity980@gmail.com>
2023-08-30 16:51:11 -0700

    Style fix

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,9 +2,8 @@
 def safe_print_division(a, b):
     try:
         result = a / b
-    except (TypeError, ValueError):
+    except (TypeError, ZeroDivisionError):
         result = None
     finally:
         print("Inside result: {}".format(result))
     return (result)
-
