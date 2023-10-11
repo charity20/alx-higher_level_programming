3d0f0ca02e12233c16f0af97978077fed6d80e70
charity mwanza <mcharity980@gmail.com>
2023-10-11 12:43:46 -0700

    style fix

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -3,6 +3,7 @@
 
 Rectangle = __import__('9-rectangle').Rectangle
 
+
 class Square(Rectangle):
     """Square class that inherits from Rectangle that inherits BaseGeometry"""
 
@@ -15,5 +16,5 @@ class Square(Rectangle):
 
     def area(self):
         """calculate square area"""
-        
+
         return self.__size ** 2
