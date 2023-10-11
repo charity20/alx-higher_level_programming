ecfc2107d3eb463cc82f0cc24efc934754b61bb6
charity mwanza <mcharity980@gmail.com>
2023-10-11 12:31:33 -0700

    fixed

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -9,9 +9,9 @@ class Square(Rectangle):
     def __init__(self, size):
         """Initialize square attrubutes"""
 
-        super().__init__(size, size)
         self.integer_validator("size", size)
         self.__size = size
+        super().__init__(size, size)
 
     def area(self):
         """calculate square area"""
