7291eee5754b8839428560d3bb9d37ce7275f716
charity mwanza <mcharity980@gmail.com>
2023-10-14 10:09:47 -0700

    updated

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,8 +7,10 @@ from sys import argv
 load_file = __import__('6-load_from_json_file').load_from_json_file
 save_file = __import__('5-save_to_json_file').save_to_json_file
 
+list = []
+
 try:
-    list = load_file('add_item.json')
+    list = list(load_file('add_item.json'))
 except (ValueError, FileNotFoundError):
     list = []
 
--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -4,16 +4,4 @@
 def class_to_json(obj):
     """check the object data type"""
 
-    if isinstance(obj, dict):
-        result = {}
-
-        for key, value in obj.items():
-            result[key] = class_to_json(value)
-        return result
-    elif isinstance(obj, list):
-        result = [class_to_json(item) for item in obj]
-        return result
-    elif isinstance(obj, (str, int, bool)):
-        return obj
-    else:
-        return str(obj)
+    return obj.__dict__
