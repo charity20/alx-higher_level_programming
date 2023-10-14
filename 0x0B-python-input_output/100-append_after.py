4b1129e1fa41e88faf53342f5dc2608c747d3c69
charity mwanza <mcharity980@gmail.com>
2023-10-14 11:23:09 -0700

    fixed

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -5,9 +5,10 @@
 def append_after(filename="", search_string="", new_string=""):
     """insert a line of text to a file"""
 
-    with open(filename, 'r+') as f:
+    with open(filename, 'r') as f:
         lines = f.readlines()
 
+    with open(filename, 'w') as f:
         for line in lines:
             f.write(line)
             if search_string in line:
