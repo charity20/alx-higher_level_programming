44c5f3a1217fc526f174ebe5b6a31f8d36ea5e71
charity mwanza <mcharity980@gmail.com>
2023-10-13 23:11:10 -0700

    task4

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
+import json
 """Return JSON representation"""
 
 
 def to_json_string(my_obj):
     """Serialize an object in JSON"""
 
-    json.dumps(my_obj)
+    return json.dumps(my_obj)
---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+import json
+
+
+def from_json_string(my_str):
+    """Return json string"""
+
+    return json.dumps(my_str)
